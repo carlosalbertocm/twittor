d7478c30f99c35adb2b1919fec789f04cfa0c67a
Carlos Alberto Conde Moreno <carlosacondemoreno@gmail.com>
2019-01-16 18:23:00 -0500

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,14 @@
+/**
+ * Guarda en el cache dinamico
+ */
+function actualizaCacheDinamico(dynamicCache, req, resp) {
+    if (resp.ok) {
+        caches.open(dynamicCache).then(cache => {
+            cache.put(req, resp.clone())
+            return resp.clone()
+        })
+    } else {
+        return resp
+    }
+
+}
